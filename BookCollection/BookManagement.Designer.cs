cbf997d406237e6d99ba7f456ff066f06d9d408a
Alex Arguelles <alexarguelles87923@gmail.com>
2024-11-24 17:18:37 -0500

    Books Page UI Done

--- BookCollection/BookManagement.Designer.cs
+++ BookCollection/BookManagement.Designer.cs
@@ -36,16 +36,25 @@
             viewToolStripMenuItem = new ToolStripMenuItem();
             tabControl1 = new TabControl();
             tabPage1 = new TabPage();
+            button2 = new Button();
+            button1 = new Button();
+            groupBox2 = new GroupBox();
+            textBox3 = new TextBox();
+            label7 = new Label();
+            label6 = new Label();
+            label5 = new Label();
+            groupBox1 = new GroupBox();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            label4 = new Label();
+            label3 = new Label();
             tabPage2 = new TabPage();
             tabPage3 = new TabPage();
-            groupBox1 = new GroupBox();
-            label3 = new Label();
-            label4 = new Label();
-            textBox1 = new TextBox();
-            textBox2 = new TextBox();
+            label8 = new Label();
             menuStrip1.SuspendLayout();
             tabControl1.SuspendLayout();
             tabPage1.SuspendLayout();
+            groupBox2.SuspendLayout();
             groupBox1.SuspendLayout();
             SuspendLayout();
             // 
@@ -72,26 +81,26 @@
             menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, eToolStripMenuItem, viewToolStripMenuItem });
             menuStrip1.Location = new Point(0, 0);
             menuStrip1.Name = "menuStrip1";
-            menuStrip1.Size = new Size(873, 42);
+            menuStrip1.Size = new Size(850, 40);
             menuStrip1.TabIndex = 2;
             menuStrip1.Text = "menuStrip1";
             // 
             // fileToolStripMenuItem
             // 
             fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            fileToolStripMenuItem.Size = new Size(71, 38);
+            fileToolStripMenuItem.Size = new Size(71, 36);
             fileToolStripMenuItem.Text = "File";
             // 
             // eToolStripMenuItem
             // 
             eToolStripMenuItem.Name = "eToolStripMenuItem";
-            eToolStripMenuItem.Size = new Size(74, 38);
+            eToolStripMenuItem.Size = new Size(74, 36);
             eToolStripMenuItem.Text = "Edit";
             // 
             // viewToolStripMenuItem
             // 
             viewToolStripMenuItem.Name = "viewToolStripMenuItem";
-            viewToolStripMenuItem.Size = new Size(85, 38);
+            viewToolStripMenuItem.Size = new Size(85, 36);
             viewToolStripMenuItem.Text = "View";
             // 
             // tabControl1
@@ -102,41 +111,91 @@
             tabControl1.Location = new Point(0, 69);
             tabControl1.Name = "tabControl1";
             tabControl1.SelectedIndex = 0;
-            tabControl1.Size = new Size(884, 871);
+            tabControl1.Size = new Size(858, 1095);
             tabControl1.TabIndex = 3;
             // 
             // tabPage1
             // 
+            tabPage1.Controls.Add(label8);
+            tabPage1.Controls.Add(textBox3);
+            tabPage1.Controls.Add(button2);
+            tabPage1.Controls.Add(button1);
+            tabPage1.Controls.Add(groupBox2);
             tabPage1.Controls.Add(label2);
             tabPage1.Controls.Add(groupBox1);
             tabPage1.Controls.Add(label1);
             tabPage1.Location = new Point(8, 46);
             tabPage1.Name = "tabPage1";
             tabPage1.Padding = new Padding(3);
-            tabPage1.Size = new Size(868, 817);
+            tabPage1.Size = new Size(842, 1041);
             tabPage1.TabIndex = 0;
             tabPage1.Text = "Books";
             tabPage1.UseVisualStyleBackColor = true;
             // 
-            // tabPage2
+            // button2
             // 
-            tabPage2.Location = new Point(8, 46);
-            tabPage2.Name = "tabPage2";
-            tabPage2.Padding = new Padding(3);
-            tabPage2.Size = new Size(868, 817);
-            tabPage2.TabIndex = 1;
-            tabPage2.Text = "Authors";
-            tabPage2.UseVisualStyleBackColor = true;
+            button2.Location = new Point(214, 935);
+            button2.Name = "button2";
+            button2.Size = new Size(172, 46);
+            button2.TabIndex = 5;
+            button2.Text = "Delete Book";
+            button2.UseVisualStyleBackColor = true;
             // 
-            // tabPage3
+            // button1
             // 
-            tabPage3.Location = new Point(8, 46);
-            tabPage3.Name = "tabPage3";
-            tabPage3.Padding = new Padding(3);
-            tabPage3.Size = new Size(868, 817);
-            tabPage3.TabIndex = 2;
-            tabPage3.Text = "Collections";
-            tabPage3.UseVisualStyleBackColor = true;
+            button1.Location = new Point(27, 935);
+            button1.Name = "button1";
+            button1.Size = new Size(172, 46);
+            button1.TabIndex = 4;
+            button1.Text = "Save Book";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(label7);
+            groupBox2.Controls.Add(label6);
+            groupBox2.Controls.Add(label5);
+            groupBox2.Location = new Point(27, 322);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(689, 292);
+            groupBox2.TabIndex = 3;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Book Properties";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(27, 697);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(689, 191);
+            textBox3.TabIndex = 0;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(43, 236);
+            label7.Name = "label7";
+            label7.Size = new Size(78, 32);
+            label7.TabIndex = 2;
+            label7.Text = "label7";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(43, 155);
+            label6.Name = "label6";
+            label6.Size = new Size(78, 32);
+            label6.TabIndex = 1;
+            label6.Text = "label6";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(43, 67);
+            label5.Name = "label5";
+            label5.Size = new Size(78, 32);
+            label5.TabIndex = 0;
+            label5.Text = "label5";
             // 
             // groupBox1
             // 
@@ -146,48 +205,77 @@
             groupBox1.Controls.Add(label3);
             groupBox1.Location = new Point(27, 76);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(761, 286);
+            groupBox1.Size = new Size(689, 211);
             groupBox1.TabIndex = 2;
             groupBox1.TabStop = false;
             groupBox1.Text = "Base Properties";
             // 
-            // label3
+            // textBox2
             // 
-            label3.AutoSize = true;
-            label3.Location = new Point(79, 73);
-            label3.Name = "label3";
-            label3.Size = new Size(42, 32);
-            label3.TabIndex = 0;
-            label3.Text = "ID:";
+            textBox2.Location = new Point(187, 137);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(200, 39);
+            textBox2.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(187, 53);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(200, 39);
+            textBox1.TabIndex = 2;
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(56, 178);
+            label4.Location = new Point(56, 137);
             label4.Name = "label4";
             label4.Size = new Size(65, 32);
             label4.TabIndex = 1;
             label4.Text = "Title:";
             // 
-            // textBox1
+            // label3
             // 
-            textBox1.Location = new Point(187, 73);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(200, 39);
-            textBox1.TabIndex = 2;
+            label3.AutoSize = true;
+            label3.Location = new Point(79, 53);
+            label3.Name = "label3";
+            label3.Size = new Size(42, 32);
+            label3.TabIndex = 0;
+            label3.Text = "ID:";
             // 
-            // textBox2
+            // tabPage2
             // 
-            textBox2.Location = new Point(187, 178);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(200, 39);
-            textBox2.TabIndex = 3;
+            tabPage2.Location = new Point(8, 46);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(842, 1041);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "Authors";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            tabPage3.Location = new Point(8, 46);
+            tabPage3.Name = "tabPage3";
+            tabPage3.Padding = new Padding(3);
+            tabPage3.Size = new Size(842, 1041);
+            tabPage3.TabIndex = 2;
+            tabPage3.Text = "Collections";
+            tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(16, 651);
+            label8.Name = "label8";
+            label8.Size = new Size(97, 32);
+            label8.TabIndex = 3;
+            label8.Text = "Authors";
             // 
             // BookManagement
             // 
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(873, 940);
+            ClientSize = new Size(850, 1158);
             Controls.Add(tabControl1);
             Controls.Add(menuStrip1);
             MainMenuStrip = menuStrip1;
@@ -198,6 +286,8 @@
             tabControl1.ResumeLayout(false);
             tabPage1.ResumeLayout(false);
             tabPage1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
             groupBox1.ResumeLayout(false);
             groupBox1.PerformLayout();
             ResumeLayout(false);
@@ -221,5 +311,13 @@
         private TextBox textBox1;
         private Label label4;
         private Label label3;
+        private GroupBox groupBox2;
+        private Button button2;
+        private Button button1;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private TextBox textBox3;
+        private Label label8;
     }
 }
