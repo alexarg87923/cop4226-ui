64679d8d963e922a835af1024d6744ee10c07795
Game PC <dominickadiaz@hotmail.com>
2024-11-24 21:37:58 -0500

    Added Everyones Names

--- BookCollection/EntryPoint.cs
+++ BookCollection/EntryPoint.cs
@@ -1,3 +1,15 @@
+
+/* 
+  Group Project - Phase 2
+
+  Made By:
+  Dominick Diaz
+  Alex Arguelles
+  Sebastian Matta
+  Orestes Soler
+ 
+ */
+
 namespace BookCollection
 {
     internal static class EntryPoint
